28b6270a4f72cc8d095a00aebab1dab307eae0dd
osintalex <63904626+osintalex@users.noreply.github.com>
2021-12-20 22:38:54 +0000

    django allowed hosts edit

--- sudan-art/django-backend/django_backend/settings.py
+++ sudan-art/django-backend/django_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG') == '1'
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
 
 
 # Application definition
