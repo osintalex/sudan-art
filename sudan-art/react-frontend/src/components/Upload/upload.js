4f20388378f04c968236d3ac3ec5a7e499d3d3ee
osintalex <63904626+osintalex@users.noreply.github.com>
2021-12-24 15:49:22 +0000

    upload loading spinner display tweak

--- sudan-art/react-frontend/src/components/Upload/upload.js
+++ sudan-art/react-frontend/src/components/Upload/upload.js
@@ -16,7 +16,7 @@ import Thumbnail from "./thumbnail.js";
 import Tagger from "./tags.js";
 import UploadFeedback from "./uploadFeedback.js";
 import { config } from "../../constants.js";
-import Emoji from "../Emoji/emoji.js"
+import Emoji from "../Emoji/emoji.js";
 
 export default function Upload() {
   const [fileValue, setFileValue] = useState("");
@@ -199,11 +199,15 @@ export default function Upload() {
                     >
                       Submit
                     </Button>
-                    {!success && submitted && !errors && (
-                      <div id="upload-emoji-spinner">
-                        <Emoji symbol="🇸🇩" fontSize="30px" />
-                      </div>
-                    )}
+                    <div
+                      id="upload-emoji-spinner"
+                      style={{
+                        display:
+                          !success && submitted && !errors ? "block" : "none",
+                      }}
+                    >
+                      <Emoji symbol="🇸🇩" fontSize="30px" />
+                    </div>
                   </FormControl>
                 </form>
               </Stack>
