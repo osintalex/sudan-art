f2bc78402362ca305914b4592f37eabfb763745d
osintalex <63904626+osintalex@users.noreply.github.com>
2022-01-30 16:05:28 +0000

    [feat]: Added unit test for Browse component

---
+++ sudan-art/react-frontend/src/components/Browse/browse.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect, useCallback } from "react";
+import NavBar from "../Navbar/navbar.js";
+import Emoji from "../Emoji/emoji.js";
+import { Button, Heading, Box, useDisclosure } from "@chakra-ui/react";
+import { config } from "../../constants.js";
+import ImageRenderer from "../ImageRenderer/imageRenderer.js";
+import ImagePopover from "../ImagePopover/imagePopover.js";
+
+/**
+ * Browse component.
+ */
+export default function Browse() {
+  const [pics, setPics] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [popoverImageDetails, setPopoverImageDetails] = useState({
+    imageSrc: "",
+    imageDescription: "",
+    imageArtist: "",
+    imageDate: "",
+  });
+
+  const getMaPics = useCallback(
+    async (page) => {
+      setLoading(true);
+      fetch(`${config.url}/recent/?page=${page}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((response) => {
+          // This bit adds more images on if there is more
+          setPics((previousImages) => {
+            return [...previousImages, ...response.results];
+          });
+          // Then handle a boolean state to track if there is indeed more below + relevant pagination
+          if (response.next) {
+            setHasMore(true);
+          } else {
+            setHasMore(false);
+          }
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    []
+  );
+  useEffect(() => {
+    getMaPics(page);
+  }, [getMaPics, page]);
+  return (
+    <>
+      <NavBar />
+      <br />
+      <div className="search-container">
+        <Box position={"relative"}>
+          <Heading>Explore Art</Heading>
+        </Box>
+      </div>
+      {loading && (
+        <div className="search-container">
+          <div id="search-spinning-emoji">
+            <Emoji symbol="🇸🇩" fontSize="30px" />
+          </div>
+        </div>
+      )}
+      <div className="search-container">
+        <div className="search-results" aria-label="search-results">
+          {pics.map((pic, index) => {
+            return (
+              <div key={`search-image-container ${index}`}>
+                <ImageRenderer
+                  className="search-image"
+                  alt={pic.artist}
+                  src={`${config.url}${pic.image}`}
+                  onClick={() => {
+                    setPopoverImageDetails({
+                      imageSrc: `${config.url}${pic.image}`,
+                      imageDescription: pic.alt_description,
+                      imageArtist: pic.artist,
+                      imageDate: pic.date_uploaded,
+                    });
+                    onOpen(true);
+                  }}
+                  aria-label="search result image"
+                />
+              </div>
+            );
+          })}
+          <ImagePopover
+            isOpen={isOpen}
+            onClose={onClose}
+            popoverImageDetails={popoverImageDetails}
+          />
+        </div>
+      </div>
+
+      {!loading && hasMore && (
+        <>
+          <div className="search-container">
+            <Button
+              height="48px"
+              width="200px"
+              id="search-button"
+              onClick={() => setPage((prevPageNumber) => prevPageNumber + 1)}
+              aria-label='has-more-button'
+            >
+              Load more
+            </Button>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
---
+++ sudan-art/react-frontend/src/components/Browse/browse.test.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import Browse from "./browse";
+import { ChakraProvider } from "@chakra-ui/react";
+
+describe("Browse Component", () => {
+  let originalFetch;
+
+  beforeEach(() => {
+    originalFetch = global.fetch;
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            count: 1,
+            next: true,
+            previous: null,
+            results: [
+              {
+                artist: "anon",
+                tags: "sudan,flag,protest",
+                image: "nothing.jpeg",
+                date_uploaded: "2021-12-24",
+              },
+            ],
+          }),
+      })
+    );
+  });
+
+  afterEach(() => {
+    global.fetch = originalFetch;
+  });
+  test("Browse loads image with has more button", async () => {
+    render(
+      <ChakraProvider>
+        <Browse />
+      </ChakraProvider>
+    );
+    await waitFor(() => {
+      expect(screen.getAllByLabelText("search-results")).toHaveLength(1);
+      expect(screen.getAllByLabelText("has-more-button")).toHaveLength(1);
+    });
+  });
+});
